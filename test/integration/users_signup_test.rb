b071d709c273cab2645f4ae8c149d62252ed1c70
Kentaro Kakimoto <kentarokakimoto@Kentaros-MacBook-Air.local>
2017-11-19 22:01:23 +0900

    comleted 11

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,8 +23,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password: "password",
                                          password_confirmation: "password"}}
     end
-      follow_redirect!
-      assert_template 'users/show'
-      assert_not flash.empty?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' }}
+    assert is logged_in?
+    delete logout_path
+    assert_not is_logged_in
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
